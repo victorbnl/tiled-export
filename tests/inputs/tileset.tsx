f3d02f7fe6000587bfecd96af5c189444543f911
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-01-01 22:32:37 +0100

    Better testing

---
+++ tests/inputs/tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="48" tileheight="48" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="48" height="48" source="img/dirt.png"/>
+ </tile>
+ <tile id="0">
+  <image width="48" height="48" source="img/grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="48" height="48" source="img/bricks.png"/>
+ </tile>
+</tileset>
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,19 +1,45 @@
 import pytest
+from pytest import mark
+
 import os
+import glob
+import re
+import filecmp
 
 from tiled_export.convert import convert
 
 
-@pytest.mark.parametrize("limit", ["finite", "infinite"])
-@pytest.mark.parametrize("encoding", ["csv", "base64"])
-@pytest.mark.parametrize("compression", ["uncompressed", "gzip", "zlib", "zstd"])
-@pytest.mark.parametrize("format_", ["csv", "json", "lua"])
-def test_map(limit, encoding, compression, format_):
+# Constants
+INPUT_FOLDER = "inputs"
+OUTPUT_FOLDER = "outputs"
+EXPECTED_OUTPUTS_FOLDER = "expected_outputs"
 
-    if encoding == "csv" and compression != "uncompressed":
-        return
 
-    filename = f"level_{limit}_{encoding}_{compression}"
+# Change to tests directory
+os.chdir(os.path.dirname(__file__))
 
-    with open(f"test_out/{filename}.{format_}", "w") as outfile:
-        outfile.write(convert(f"tests/files/{filename}.tmx", format_))
+# Create output directory
+if not os.path.exists(OUTPUT_FOLDER):
+    os.mkdir(OUTPUT_FOLDER)
+
+
+@mark.parametrize("filename", [os.path.basename(path) for path in glob.glob(f"{INPUT_FOLDER}/*") if re.match(r".*\.[tmx|tsx]", path)])
+@mark.parametrize("format_", ["csv", "json", "lua"])
+def test_all(filename, format_):
+
+    # Split base name and extension
+    name, ext = os.path.splitext(filename)
+
+    # Cannot export tileset as CSV
+    if format_ == "csv" and ext == ".tsx":
+        pytest.skip("Cannot export tileset as CSV")
+
+    # Expected output not yet written
+    if not os.path.exists(f"{EXPECTED_OUTPUTS_FOLDER}/{name}.{format_}"):
+        pytest.xfail(f"Expected output not yet written for file: {name}.{format_}")
+
+    # Convert the file
+    convert(f"{INPUT_FOLDER}/{filename}", format_, f"{OUTPUT_FOLDER}/{name}.{format_}")
+
+    # Check if output file is equal to expected output
+    assert filecmp.cmp(f"{OUTPUT_FOLDER}/{name}.{format_}", f"{EXPECTED_OUTPUTS_FOLDER}/{name}.{format_}")
